9136b7b826c83e0c8eda348729d4112244cb5783
amrashrv <a.shrv94@gmail.com>
2021-11-16 14:18:17 +0300

    backStructure

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors')
 const taskRouter = require('./src/routes/tasks-router')
 
 const uri = `mongodb+srv://amir:user@cluster0.5y4qx.mongodb.net/tasks?retryWrites=true&w=majority`
 const app = express();
 const port = process.env.HOST_PORT || 5000;
 app.use(express.json());
+app.use(cors())
 
 app.use('/tasks', taskRouter);
 
