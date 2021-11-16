bd8f08334b53e2378a34dbf91d00de6f91460d79
amrashrv <a.shrv94@gmail.com>
2021-11-16 12:28:53 +0300

    realized connection to mongoDB

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
+const uri = `mongodb+srv://amir:user@cluster0.5y4qx.mongodb.net/tasks?retryWrites=true&w=majority`
+const app = express();
+const port = process.env.HOST_PORT || 5000
+
+
+
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+  try{
+    mongoose.connect(uri)
+    console.log('server connected to DB')
+  } catch (e) {
+    console.log(e)
+  }
+
+})
